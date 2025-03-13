fd54870a748e313eea3f64cabcc646adbe50088b
Ryan <ryandodddeveloper@gmail.com>
2025-03-12 20:48:09 -0500

    Major overwrite (#17)
    * Major overwrites for mobile and desktop support
    
    * Fixes favicon
    
    * Fix so social posts open in new tab
    
    * Last fix for #16

---
+++ src/app/components/Section.tsx
@@ -0,0 +1,14 @@
+export function Section({
+	color,
+	children,
+}: {
+	color: string;
+	children: React.ReactNode;
+}) {
+	return (
+		<div
+			className={`${color} ${color === "bg-magnolia" && "text-black"} ${color === "bg-magenta" && "text-white"} flex flex-col items-center justify-center gap-8 py-[2rem]`}>
+			{children}
+		</div>
+	);
+}
